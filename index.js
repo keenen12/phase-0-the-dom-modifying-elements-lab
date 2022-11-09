d365094dd59448f5c9c2ff0c740f176ed3f4b33d
Keenen Harral <kharral23@yahoo.com>
2022-11-08 20:02:54 -0800

    commit tree 960309294a93b5a0f349aa4d0f967bb487dd8a2d

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const body = document.getElementsByTagName('body')[0];
+let main = document.getElementById('main');
+body.removeChild(main);
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute("id", 'victory');
+
+newHeader.textContent = "Keenen is the champion";
